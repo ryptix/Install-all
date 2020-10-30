5b9822c9e33177ee48dd07413cc0d73ffc0a53f1
ryptix <ryptix@protonmail.com>
2020-10-30 16:14:18 +0000

    pkglist.txt && installer

--- install-all.go
+++ install-all.go
@@ -5,19 +5,39 @@ import (
 	"io"
 	"log"
 	"os/exec"
+    "os"
+    "strconv"
 )
 
 func main() {
     fmt.Println("Hey Friend :3");
+    if getProcessOwner() != "root\n" {
+        log.Fatalln("Sorry you need to be root");
+    }
+
     fmt.Println("Let's start by updating your system");
     
     cmd := exec.Command("pacman","-Syyyu","--noconfirm");
     stdout, err := cmd.StdoutPipe();
     if err != nil {log.Fatalln(err);}
 
-    if err := cmd.Start(); err != nil {
-		log.Fatal(err);
-	}
+    if err := cmd.Start(); err != nil {log.Fatal(err);}
+
+    readBuffer(stdout);
+    
+    if len(os.Args) >= 1{
+    
+    }else {
+        installBasePackage();
+    }
+}
+
+func installBasePackage(){
+    cmd := exec.Command("pacman", "-S","--noconfirm","--needed","$(comm -12 <(pacman -Slq|sort) <(sort pkglist.txt))");
+    
+    stdout, err := cmd.StdoutPipe();
+    if err != nil {log.Fatalln(err);}
+    if err := cmd.Start(); err != nil {log.Fatal(err);}
 
     readBuffer(stdout);
 }
@@ -32,6 +52,11 @@ func readBuffer(buff io.ReadCloser){
         // byte to string
         str := string(data[:n]);
         fmt.Print(str);
-
     }
+}
+
+func getProcessOwner() string {
+    stdout, err := exec.Command("ps", "-o", "user=", "-p", strconv.Itoa(os.Getpid())).Output()
+    if err != nil {log.Fatalln(err);}
+    return string(stdout)
 }
